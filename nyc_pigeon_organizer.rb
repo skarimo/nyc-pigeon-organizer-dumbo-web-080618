e5285f62bcf82d9099dcd10a6ca1decb30ef59f8
Sherzod Karimov <karimovj@gmail.com>
2018-07-24 18:34:40 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,11 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |k, v|
-    v.each do |att, name|
-      new_hash[name] = {}
+    v.each do |att, names|
+      names.each do |name|
+        new_hash[name] = {}
+    end
   end
  end
+ puts new_hash
 end
