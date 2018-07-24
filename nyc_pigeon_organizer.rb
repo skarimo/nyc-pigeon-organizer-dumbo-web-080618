6b83166cdd6abe9818f2b1683a39415b6e084048
Sherzod Karimov <karimovj@gmail.com>
2018-07-24 19:01:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |k, v|
+    v.each do |att, names|
+      names.each do |name|
+        new_hash[name] = {} unless new_hash.include?(name)
+        new_hash[name][k] = [] unless new_hash[name][k] != nil
+          if names.include?(name)
+             new_hash[name][k] << att.to_s
+          end
+    end
+  end
+ end
+new_hash
 end
